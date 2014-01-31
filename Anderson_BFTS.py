ed87794a853adc04fc0b6322f43d5808d2baa999
Mathew Anderson <anders30@gmail.com>
2014-01-30 21:36:27 -0600

    Initial main

--- Anderson_BFTS.py
+++ Anderson_BFTS.py
@@ -2,3 +2,16 @@
 # @author Mathew Anderson
 # @brief Implementation of main method
 # for CS347SP14 Puzzle Project part I - BFTS
+
+from PuzzleReader import ReadPuzzle
+
+from WrigglerReader import FindWrigglers
+
+# prompt for file name
+puzzleFile = raw_input('Enter filename of puzzle: ')
+
+# attempt to construct the initial state
+initialState = ReadPuzzle(puzzleFile)
+
+# attempt to extract all wriggler info
+wrigglers = FindWrigglers(puzzle)
